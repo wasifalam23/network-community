d43b5434304fb963a32f0b73977831bb36605edc
wasif_alam <wsfalam84@gmail.com>
2024-05-08 12:52:04 +0530

    add two more size options for images config

--- utils/getMemberStyle.js
+++ utils/getMemberStyle.js
@@ -1,6 +1,8 @@
 const sizes = [
-	{ name: 'xl', style: 'w-20 h-20' },
-	{ name: 'lg', style: 'w-16 h-16' },
+	{ name: 'xl', style: 'w-16 h-16' },
+	{ name: 'lg', style: 'w-14 h-14' },
+	{ name: 'md', style: 'w-12 h-12' },
+	{ name: 'sm', style: 'w-11 h-11' },
 ];
 
 const getMemberStyle = (sizeName, angleName) => {
